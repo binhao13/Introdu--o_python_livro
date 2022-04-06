c52cfbd73d9adc900e9afeae62eee8b47d8cfd93
binhao13 <luizfelipebarros@alunos.utfpr.edu.br>
2022-04-06 20:44:08 -0300

    adf

---
+++ 3_5.py
@@ -0,0 +1,4 @@
+# a < b -> false,portanto false and true or false -> false or false -> false
+# a > b -> true,portanto true and false or false -> false or false -> false
+# a > b -> true,portanto true and true or true -> true and true -> true
+    
