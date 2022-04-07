5e125a8ae6784940846b5b502980c9ea80660d71
binhao13 <luizfelipebarros@alunos.utfpr.edu.br>
2022-04-07 16:39:04 -0300

    ad

--- 3_12.py
+++ 3_12.py
@@ -1,4 +1,4 @@
-d = float(input("Digite a distância: "))
-v = float(input("Digite a velocidade média: "))
+d = float(input("Digite a distância (m): "))
+v = float(input("Digite a velocidade média (m/s): "))
 t = d/v
 print(f"O tempo de viagem é: {t} s ")
