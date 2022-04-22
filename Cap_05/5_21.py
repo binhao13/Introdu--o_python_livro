763d27b932c521fd173db2c2a923ad75aa93f503
binhao13 <you@example.com>
2022-04-22 12:01:23 -0300

    picle

---
+++ Cap_05/5_21.py
@@ -0,0 +1,27 @@
+from operator import truediv
+
+
+while True:
+    valor = int(input("Digite o valor a pagar: "))
+    if valor == 0:
+        break
+    cédulas = 0
+    atual = 50
+    apagar = valor
+    while True:
+        if atual <= apagar:
+            apagar -= atual
+            cédulas += 1
+        else:
+            print(f"{cédulas} cédula(s) de R${atual}")
+            if apagar == 0:
+                break
+            if atual == 50:
+                atual = 20
+            elif atual == 20:
+                atual = 10
+            elif atual == 10:
+                atual = 5
+            if atual == 5:
+                atual = 1
+            cédulas  = 0
