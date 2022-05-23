1af7900332fd66f2ecfcd098f0efb2647061874e
binhao13 <luizfelipearagaodebarros@gmail.com>
2022-05-23 20:57:49 -0300

    SDSA

--- Cap_06/6_07.py
+++ Cap_06/6_07.py
@@ -1,4 +1,20 @@
-parenteses = 0
-pilha = list(range(0, parenteses + 1))
-while True:
-    
+expressão = input("Digite a sequência de parênteses a validar:")
+x = 0
+pilha = []
+while x < len(expressão):
+    if expressão[x] == "(":
+        pilha.append("(")
+    if expressão[x] == ")":
+        if len(pilha) > 0:
+            topo = pilha.pop(-1)
+        else:
+            pilha.append(")")  # Força a mensagem de erro
+            break
+    x = x + 1
+if len(pilha) == 0:
+    print("OK")
+else:
+    print("Erro")
+
+   
+
