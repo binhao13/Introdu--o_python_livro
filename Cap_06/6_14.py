9245cff1f9cf20e8b98d57498941248738fec557
binhao13 <you@example.com>
2022-05-24 17:50:27 -0300

    sd

---
+++ Cap_06/6_14.py
@@ -0,0 +1,3 @@
+# Se a lista já estiver ordenada, nenhum elemento é maior que o elemento seguinte.
+# Desta forma, após a primeira verificação de todos os elementos,
+# o loop interno é interrompido pela condição de (9).
