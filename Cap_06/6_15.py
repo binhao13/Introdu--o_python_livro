17e3cd685ff29e599c4b49fa0b13ee00211642b4
binhao13 <you@example.com>
2022-05-24 17:51:23 -0300

    rf

---
+++ Cap_06/6_15.py
@@ -0,0 +1,4 @@
+# Como utilizamos o método de bolhas, na primeira verificação, 3, 3 são considerados como na ordem correta.
+# Quanto verificamos o segundo 3 com 1, ocorre uma troca.
+# O mesmo vai ocorrer com o primeiro 3, mas apenas na próxima repetição. Veja que o 1 subiu para a primeira posição
+# como uma bolha de ar dentro d'água.
