9aa106f9819318864ca469a7bbe906bcb0f8748f
binhao13 <you@example.com>
2022-05-24 15:12:59 -0300

    ugu

--- Cap_06/6_10.py
+++ Cap_06/6_10.py
@@ -2,27 +2,27 @@ l = [15,7,27,39]
 p = int(input("Digite o valor a procurar: "))
 v = int(input("Digite outro valor a procurar: "))
 x = 0
-y = 100
-z = 100
+y = 0
+z = 0
 achou1 = False
 achou2 = False
 while x < len(l):
     if l[x] == p:
-        print(f"p:{p} encontrado")
+        print(f"{p} achado na posição {x}")
         achou1 = True
         y = x
     elif l[x] == v:
-        print(f"v:{v} encontrado")
+        print(f"{v} achado na posição {x}")
         achou2 = True
         z = x
     elif achou1 and achou2:
         break
     x += 1
 if achou1 == False:
-    print(f"p: {p} não encontrado")
+    print(f"{p} não encontrado")
 elif achou2 == False:
-    print(f"v: {v} não encontrado")
-if y < z and achou1:
+    print(f"{v} não encontrado")
+if y < z:
     print(f"p foi achado primeiro")
-elif achou2:
+else:
     print(f"v foi achado primeiro")
