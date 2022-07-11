a9af0f4dc94bc232dcbd99a2ae5dd143a97357de
binhao13 <luizfelipearagaodebarros@gmail.com>
2022-07-11 15:52:28 -0300

    egry

---
+++ Cap_07/7_04.py
@@ -0,0 +1,13 @@
+from this import d
+
+
+s1 = input("Digite uma string: ")
+q = {}
+x = 0
+for letra in s1:
+    if letra in q:
+        q[letra] = q[letra] + 1    
+    else:
+        q[letra] = 1
+for letra,valor in q.items():
+    print(f"{letra}: {valor}x ")
